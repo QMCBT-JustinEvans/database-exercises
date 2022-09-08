75a1fd49353966d924f379f7362ef2268a3a29a0
QMCBT-JustinEvans <Justin.Ellis.Evans@gmail.com>
2022-09-08 12:07:35 -0500

    Upload NOTES for personal reference

---
+++ NOTES_Clauses.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date = '1993-11-19';
+
+SELECT DISTINCT first_name FROM employees WHERE first_name LIKE 'sus%';
+
+SELECT DISTINCT first_name FROM employees WHERE first_name BETWEEN 'A' AND 'D';
+-- BETWEEN is inclusive on both sides with Numbers but exclusive on end with Letters
+
+SELECT DISTINCT first_name FROM employees WHERE first_name BETWEEN 'AD' AND 'DF';
+
+SELECT * FROM employees WHERE first_name IN ('Herbert', 'Zvonko');
+
+SELECT * FROM zillow.properties_2016
+WHERE basementsqft IS NOT NULL;
+
+SELECT * FROM fruits_db.fruits WHERE id > 4 AND quantity > 5;
